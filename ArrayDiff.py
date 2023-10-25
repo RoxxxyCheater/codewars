4a4fb40bbe5152ee6b4157cc56d995b25355be0a
@Skysoul <71335149+RoxxxyCheater@users.noreply.github.com>
2023-10-25 22:32:23 +0200

    [0] task

---
+++ ArrayDiff.py
@@ -0,0 +1,18 @@
+# 6 kyu
+# Array.diff
+# 347163288% of 23,920101,134 of 243,513marcinbunsch2 Issues Reported
+#  Python
+# 3.11
+# VIM
+# EMACS
+# Instructions
+# Output
+# Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
+
+# It should remove all values from list a, which are present in list b keeping their order.
+
+# array_diff([1,2],[1]) == [2]
+# If a value is present in b, all of its occurrences must be removed from the other:
+
+# array_diff([1,2,2,2,3],[2]) == [1,3]
+# ARRAYSFUNDAMENTALSALGORITHMS
