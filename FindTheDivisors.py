2f56f05abd6688353e8a236d45cb4e5bfc0e8a32
@Skysoul <71335149+RoxxxyCheater@users.noreply.github.com>
2023-10-26 23:35:54 +0200

    [1] func task

--- FindTheDivisors.py
+++ FindTheDivisors.py
@@ -13,3 +13,20 @@
 # divisors(12); #should return [2,3,4,6]
 # divisors(25); #should return [5]
 # divisors(13); #should return "13 is prime"
+
+
+
+def divisors(n):
+  return res
+
+
+    def basic_test_cases():
+        return True if divisors(15) == [3,5])      
+        return True if divisors(253) == [11,23]
+        return True if divisors(24) == [2,3,4,6,8,12]
+        return True if divisors(25) == [5]
+        return True if divisors(13) == "13 is prime"
+        return True if divisors(3) == "3 is prime"
+        return True if divisors(29) == "29 is prime"
+
+basic_test_cases()
